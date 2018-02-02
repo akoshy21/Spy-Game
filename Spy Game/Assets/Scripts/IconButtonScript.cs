2472dad4ab94e3911e92493964cf0a2fb3371766
Unknown <ak6429@nyu.edu>
2018-02-02 13:06:51 -0500

    opening apps works now

---
+++ Spy Game/Assets/Scripts/IconButtonScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class IconButtonScript : MonoBehaviour {
+
+	public string buttonApp;
+
+	public bool open = false;
+
+	// Use this for initialization
+	void Start () {
+		Button btn = this.GetComponent<Button> ();
+		btn.onClick.AddListener (TaskOnClick);
+	}
+
+	void TaskOnClick () {
+		if (open == false) {
+			SceneManager.LoadScene (buttonApp, LoadSceneMode.Additive);
+			open = true;
+		}
+		else if (open == true)
+		{
+			SceneManager.UnloadSceneAsync (buttonApp);
+			open = false;
+		}
+	}
+}
